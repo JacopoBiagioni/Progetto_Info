59f304380bc4b97f96b2bded13e4c4ccc397d053
sasyvelobelo <ambrosino.salvatore@iisgalvanimi.edu.it>
2022-05-04 11:49:25 +0000

    oi

--- progetto.py
+++ progetto.py
@@ -14,26 +14,28 @@ import pandas as pd
 covid = pd.read_csv('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv')
 regioni =  gpd.read_file('/workspace/Progetto_Info/Reg01012021_g_WGS84.zip')
 province = gpd.read_file('/workspace/Progetto_Info/ProvCM01012021_g_WGS84.zip')
+print(regioni)
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
 
 @app.route('/regione/<nome_regione>', methods=['GET'])
 def regione(nome_regione):
+    global dati_regione,province_regione
     dati_regione = regioni[regioni["DEN_REG"] == nome_regione]
     confini_regione = regioni[regioni.touches(dati_regione.geometry.squeeze())]
     province_regione = province[province.within(dati_regione.geometry.squeeze())]
 
-    return render_template('visualizza_regione.html',regione=nome_regione,confini=confini_regione.DEN_REG.to_list())
+    return render_template('visualizza_regione.html',regione=nome_regione,confini=confini_regione.DEN_REG.to_list(),province_regione=province_regione.DEN_PROV.to_list())
 
 
-@app.route('/regione.png', methods=['GET'])
-def liguriapng():
+@app.route('/mappa.png', methods=['GET'])
+def regionepng():
     fig, ax = plt.subplots(figsize = (12,8))
 
    
-    Pliguria.to_crs(epsg=3857).plot(ax=ax,facecolor='none', edgecolor="r")
-    liguria.to_crs(epsg=3857).plot(ax=ax,facecolor='none', edgecolor="k")
+    province_regione.to_crs(epsg=3857).plot(ax=ax,facecolor='none', edgecolor="r")
+    dati_regione.to_crs(epsg=3857).plot(ax=ax,facecolor='none', edgecolor="k")
     contextily.add_basemap(ax=ax)  
 
     output = io.BytesIO()
